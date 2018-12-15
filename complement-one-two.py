6a164b4e6e4851a239de2436c1a93ce077af7638
valdinei <valdineifelipe811@gmail.com>
2018-12-15 13:13:26 -0300

    improved code

--- complement-one-two.py
+++ complement-one-two.py
@@ -1,28 +1,30 @@
-def complemento_1(numero):	
-	for i in range(len(numero)):
-		numero[i] = "1" if numero[i] == "0" else "0"
-	
-	return "".join(numero)
+import os
 
-def complemento_2(numero):
-	c1 = list(complemento_1(numero))
-	for i in range(len(numero)-1,0,-1):
-		if c1[i] == "1":
-			c1[i] = "0"
+def complemento_1(num):	
+	for i in range(len(num)):
+		num[i] = "1" if num[i] == "0" else "0"
+	
+	return "".join(num)
+
+def complemento_2(num):
+	num = list(complemento_1(num))
+	for i in range(len(num)-1,-1,-1):
+		if num[i] == "1":
+			num[i] = "0"
 		else:
-			c1[i] = "1"
+			num[i] = "1"
 			break
 	if i == -1:
-		c1 += "1"
+		num += "1"
 	
-	return "".join(c1)
+	return "".join(num)
 
-	
-		
-numero = []
-entrada = []
-entrada = list(input("Digite um número binário:"))
-numero = entrada[:]
-print('complemento 1:',complemento_1(numero))
-numero = entrada[:]
-print('complemento 2:',complemento_2(numero))
+op = "n"
+while op != "s":	
+	entrada = list(input("Digite um número binário:"))
+	num = entrada[:]
+	print('complemento 1:',complemento_1(num))
+	num = entrada[:]
+	print('complemento 2:',complemento_2(num))
+	op = input("Deseja sair? (s ou n):")
+	os.system("clear")
